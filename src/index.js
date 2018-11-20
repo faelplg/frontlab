79b9787a9f51ec29788aa028b21e127de3e40d7d
faelplg <faelplg@gmail.com>
2018-11-19 22:11:17 -0200

    This is a huge step for an ant.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './base.scss';
+
+const converter = new showdown.Converter({tables: true});
+const _url_ = 'https://api.github.com/repos/faelplg/web-development-researches/readme'
+
+fetch(_url_, {
+  headers: {
+    Accept: 'application/vnd.github.v3.raw+json'
+  }
+}).then(response => {
+  return response.text();
+}).then(result => {
+  const _content_ = converter.makeHtml(result);
+  console.log('_content_', _content_);
+  document.getElementById('web-development-researches').innerHTML = _content_;
+});
